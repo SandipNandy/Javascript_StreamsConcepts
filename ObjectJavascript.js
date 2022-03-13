ad1bfe1725b0d372ba35a4a15d18fa97ecef55af
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-03-13 13:16:00 +0530

    1st commit

---
+++ ObjectJavascript.js
@@ -0,0 +1,22 @@
+let person={
+    FirstName:'Sandip',
+    LastName:'Nandi',
+    Age: 26,
+    InbuildFunction: function(A,B){
+       console.log(A+B);
+       console.log(this.FirstName+this.LastName);
+    }
+}
+console.log(person.InbuildFunction(5,6));
+
+person.LastName='Nandy'
+person.gender='Male'
+console.log(person.FirstName);
+console.log(person.LastName);
+console.log(person);
+console.log('gender' in person);
+for(let i in person){
+    console.log(person[i]);
+}
+
+
