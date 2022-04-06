fe6efd48e11d0f07c3f0c5fd7267e38b4312c61f
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-04-06 13:02:01 +0530

    1st Commit

---
+++ DeleteExcelFileFromAtargeted_Directory.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const dirPath="C:/Users/MyFile";
+const t=/^[a-z0-9][-a-z0-9\x20_!()\.:,]*\.xlsx?$/i;
+var fileNames = fs.readdirSync(dirPath);
+console.log(fileNames);
+const ExcelFileName=fileNames.filter(elem=> elem.match(t));
+const pathToFile = "./"+ExcelFileName+"";
+fs.unlink(pathToFile, function(err) {
+  if (err) {
+    throw err
+  } else {
+    console.log("Successfully deleted the file.")
+  }
+})
