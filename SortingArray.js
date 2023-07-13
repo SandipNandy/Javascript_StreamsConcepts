fd001e6e63bbfcd6bf65c1ea3c7843a81defd88b
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-13 09:19:33 +0530

    Update SortingArray.js

--- SortingArray.js
+++ SortingArray.js
@@ -2,6 +2,8 @@
 let fruits=['Guava','Pomegranate','Apple','Banana','Mango']
 //here sring will be sorting but make sure all the letter should be either 
 //in capital or small or fisrt letter should be capital for all strings available in the array.
+
+
 fruits.sort();
 console.log(fruits);
 let numbers=[87,65,32,90,62,73,12,76];
