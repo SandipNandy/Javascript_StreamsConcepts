61db53cb1ae5d05232422e2e9a5f79d1281fa985
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-08-06 21:11:56 +0530

    06Aug 1st commit
    Delete excel file successfully.

--- DeleteExcelFileFromAtargeted_Directory.js
+++ DeleteExcelFileFromAtargeted_Directory.js
@@ -1,14 +1,11 @@
-
-//Here we want to deleted a specific Excel file with '.xlsx' extension from a target directory
 const fs = require("fs");
 const dirPath="C:/Users/MyFile";
-const t=/^[a-z0-9][-a-z0-9\x20_!()\.:,]*\.xlsx?$/i;
-var fileNames = fs.readdirSync(dirPath);
+const filetype=/^[a-z0-9][-a-z0-9\x20_!()\.:,]*\.xlsx?$/i;
+const fileNames = fs.readdirSync(dirPath);
 console.log(fileNames);
-const ExcelFileName=fileNames.filter(elem=> elem.match(t));
+const ExcelFileName=fileNames.filter(elem=> elem.match(filetype));
 const pathToFile = "./"+ExcelFileName+"";
 fs.unlink(pathToFile, function(err) {
-  
   if (err) {
     throw err
   } else {
