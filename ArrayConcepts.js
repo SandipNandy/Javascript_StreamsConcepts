74b04e7b68dc04903494a27436d76a4aa4136b6c
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-08-18 09:26:22 +0530

    Update ArrayConcepts.js

--- ArrayConcepts.js
+++ ArrayConcepts.js
@@ -41,6 +41,7 @@ console.log(marks.length);
 marks.push(65);
 
 //in 'marks' array it will add the number at end side
+
 console.log(marks);
 console.log(marks.length);
 marks.pop();// delect the element of the array from the last
