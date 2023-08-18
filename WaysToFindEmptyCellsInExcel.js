200c6e7f738db2dc335e0a55a24a4ccaa522bca9
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-08-18 09:27:58 +0530

    Update WaysToFindEmptyCellsInExcel.js

--- WaysToFindEmptyCellsInExcel.js
+++ WaysToFindEmptyCellsInExcel.js
@@ -1,10 +1,8 @@
 
     const filePath = FILEPATH;
     //const workbookHeaders = XLSX.readFile(filePath, { sheetRows: 1 });
-
     let cvt = function (n) { return (String.fromCharCode(n + 'A'.charCodeAt(0) - 1)) }
     const workbookHeaders = XLSX.readFile(filePath);
-
     let HeaderRow;
     let ColumnHeaderNames = [];
 
