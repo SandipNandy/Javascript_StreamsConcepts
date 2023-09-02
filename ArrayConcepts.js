bca3e458e2e3e31741a9639b80318be58955897a
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-02 07:41:29 +0530

    Update ArrayConcepts.js

--- ArrayConcepts.js
+++ ArrayConcepts.js
@@ -9,6 +9,7 @@ let marks=Array(6);
 var marks=new Array(20,40,35,12,37,100);//here it will through and error as we already delaired 'marks'
 
 with 'let' keyword but again in with 'var' we are declaring same 'marks' variable which 'let' will not accept
+
 so this will through compile time error
 
 var mark=[];// here it is allowed to give same name with 'var' variable.
