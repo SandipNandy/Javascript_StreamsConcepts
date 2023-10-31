5ca5e8cc6c8b2df739b11821305b51ecffcbab35
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-10-31 09:20:45 +0530

    Update ArrayConcepts.js

--- ArrayConcepts.js
+++ ArrayConcepts.js
@@ -29,7 +29,6 @@ marks.push(65);
 console.log(marks);
 console.log(marks.length);
 marks.pop();// delect the element of the array from the last
-
 console.log(marks);
 marks.unshift(98);//it will add the elements of the array in the first
 console.log(marks);
