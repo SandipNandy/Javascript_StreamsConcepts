a6013e52c20e1c750615de506c9a8db40de629b4
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-01-03 08:21:31 +0530

    Update DeleteExcelFileFromAtargeted_Directory.js

--- DeleteExcelFileFromAtargeted_Directory.js
+++ DeleteExcelFileFromAtargeted_Directory.js
@@ -4,7 +4,6 @@ const filetype=/^[a-z0-9][-a-z0-9\x20_!()\.:,]*\.xlsx?$/i;
 const fileNames = fs.readdirSync(dirPath);
 console.log(fileNames);
 const ExcelFileName=fileNames.filter(elem=> elem.match(filetype));
-
 const pathToFile = "./"+ExcelFileName+"";
 
 fs.unlink(pathToFile, function(err) {
